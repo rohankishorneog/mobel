55ef1e0821eeb79547216f69aa120c2d63b84e2d
rohankishorneog <rkn2601@gmail.com>
2023-08-22 20:06:20 +0530

    added routing paths and minor css changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import RotatingLinks from "./Components/RotatingLink/RotatingLinks";
-
+//  import Mockman from  'mockman-js'
 
 import { Routes,Route } from "react-router-dom";
 import  SingleProduct  from "./Components/SingleProduct/SingleProduct";
@@ -10,6 +10,12 @@ import SignupPage from "./Pages/signupPage/SignupPage";
 import RequiresAuth from "./Components/RequiresAuth"
 import {Cart} from "./Components/Cart/Cart"
 import {Wishlist }from "./Components/Wishlist/Wishlist"
+import SingleCategory from "./Components/SingleCategory/SingleCategory";
+import Profile from "./Components/Profile/Profile";
+import Address from "./Components/Address/Address";
+import CheckOutPage from "./Pages/checkoutPage/CheckOutPage";
+import AllProducts from "./Components/AllProducts/AllProducts";
+import NavBar from "./Components/NavBar/NavBar";
 
 
 
@@ -17,19 +23,28 @@ function App() {
   return (
     <div className="App">
           <RotatingLinks/>
+          <NavBar/>
+          {/* <Mockman/> */}
           <Routes>
           <Route path="/" element={<Landing/>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/signup' element={<SignupPage/>}/>
             <Route path="/newCollection" element={<div>New element</div>}/>
             <Route path="/rugCollection" element={<div>Rug element</div>}/>
+            <Route path="/products" element={<AllProducts/>}/>
             <Route path="/products/:id" element={<SingleProduct/>}/>
+            <Route path="/categories/:categoryName" element={<SingleCategory/>}/>
              <Route path='/wishlist' element={
           <RequiresAuth><Wishlist/></RequiresAuth>}/>
            <Route path='/cart' element={
           <RequiresAuth><Cart/></RequiresAuth>}/>
-           {/* <Route path='/profile' element={
-          <RequiresAuth><Profile/></RequiresAuth>}/> */}
+           <Route path='/profile' element={
+          <RequiresAuth><Profile/></RequiresAuth>}/>
+          <Route path='/address' element={
+          <RequiresAuth><Address/></RequiresAuth>}/>
+        
+          <Route path='/checkout' element={
+          <RequiresAuth><CheckOutPage/></RequiresAuth>}/>
           </Routes>
 
     </div>
