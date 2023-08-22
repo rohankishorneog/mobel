36b529fe766e105b49bb61e65acc6d417b846341
rohankishorneog <rkn2601@gmail.com>
2023-08-22 20:16:04 +0530

    added toast fixed minor bug

--- src/Components/Buttons/WishlistButton/WishListButton.jsx
+++ src/Components/Buttons/WishlistButton/WishListButton.jsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import { WishlistContext } from "../../../contexts/WishlistContext";
+import { toast } from "react-toastify";
+import "./WishListButton.css"
 
- const WishListButton = ({ product }) => {
-  const { addItemsToWishlist } = useContext(WishlistContext);
+ const WishListButton = ({ newProduct }) => {
+  const { wishlistContents,addItemToWishlist } = useContext(WishlistContext);
 
   const handleWishlist = () => {
-    addItemsToWishlist(product);
+    if(wishlistContents.find(content=>content._id===newProduct._id)){
+      toast.error('item already added')
+    }else{
+      addItemToWishlist(newProduct);
+    }
+    
   };
 
-  return <button onClick={handleWishlist}>Add to wishlist</button>;
+  return <button className="wishlist-button"onClick={handleWishlist}>ADD TO WISHLIST</button>;
 };
 
 
