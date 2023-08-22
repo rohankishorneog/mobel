2b2286b6f468d94122f596b78e0a0ee556548005
rohankishorneog <rkn2601@gmail.com>
2023-08-22 20:21:38 +0530

    created and css added

--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect   } from "react";
-import { useParams } from "react-router-dom"; // Import the useParams hook
+import { Link, useParams } from "react-router-dom"; // Import the useParams hook
+import './SingleProduct.css'
 import { ProductContext } from "../../contexts/ProductContext"; // Make sure to import the context from the correct file
+import { Cartbutton } from "../Buttons/CartButton/CartButton";
+import WishListButton from "../Buttons/WishlistButton/WishListButton";
+import NavBar from "../NavBar/NavBar";
 
 const SingleProduct = () => {
   const { id} = useParams(); // Access the "productId" from the URL parameter
-  console.log(id)
   const { selectedProduct, getProduct } = useContext(ProductContext);
 
   // Fetch the selected product when the component mounts or when the "productId" changes
   useEffect(() => {
     getProduct(id);
-  }, [id,getProduct]);
+  }, [id]);
 
   // Show loading message while fetching data
   if (!selectedProduct) {
@@ -20,11 +23,30 @@ const SingleProduct = () => {
   // Display the product information once data is available
   return (
     <div>
-      <h2>{selectedProduct.name}</h2>
+    <div className="single-product-main-div">
+       <div className="single-product-card">
+       <img src={selectedProduct.imageUrl} alt={selectedProduct.title} />
+       <div className="single-rpoduct-details">
+       <h3>{selectedProduct.title}</h3>
+      
+      <p>Rs{selectedProduct.price}</p>
+      <div className="add-buttons"> 
+      <Cartbutton newProduct={selectedProduct}/>
+      <WishListButton newProduct={selectedProduct}/>
+      <hr/>
+      
+      <h4>Description</h4>
+    
       <p>{selectedProduct.description}</p>
-      <p>Price: ${selectedProduct.price}</p>
-      {/* Add more details as needed */}
+
+      </div>
+      
+       </div>
     </div>
+
+    </div>
+
+   </div>
   );
 };
 
