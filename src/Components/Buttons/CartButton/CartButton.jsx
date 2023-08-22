b3bc0519d8b92d399456b0bcffe7f5747325e3b1
rohankishorneog <rkn2601@gmail.com>
2023-08-22 20:15:24 +0530

    added toast when incremented

--- src/Components/Buttons/CartButton/CartButton.jsx
+++ src/Components/Buttons/CartButton/CartButton.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react";
-
 import { CartContext } from "../../../contexts/CartContext";
+import './CartButton.css'
+import { toast } from "react-toastify";
 
-export const Cartbutton = ({ product }) => {
-  const { addItem } = useContext(CartContext);
+export const Cartbutton = ({ newProduct }) => {
+  console.log(newProduct);
+  const { cartContents, addItem, increment } = useContext(CartContext);
 
   const handleAddToCart = () => {
-    addItem(product);
+    const existingProduct = cartContents.find((item) => item._id === newProduct._id);
+
+    if (existingProduct) {
+     
+      increment(newProduct._id);
+      toast.success("Incrementing product")
+    } else {
+    
+      addItem(newProduct);
+    }
   };
+
   return (
     <div>
-      <button onClick={handleAddToCart}>Add to Cart</button>
+      <button className="cart-button" onClick={handleAddToCart}>ADD TO CART</button>
     </div>
   );
 };
