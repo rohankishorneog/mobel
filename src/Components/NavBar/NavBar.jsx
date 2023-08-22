60638a9383ca20c175ec764a9097715ed165e258
rohankishorneog <rkn2601@gmail.com>
2023-08-22 20:18:10 +0530

    added home button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./NavBar.css"
+import { AuthContext } from '../../contexts/AuthContext'
+import { Link } from 'react-router-dom'
+import { FaHeart,  FaHome,  FaShoppingCart, FaUser } from 'react-icons/fa'
 
 const NavBar = () => {
+  const {loggedInUser}=useContext(AuthContext)
   return (
     <div className='main-div'>
+        <div>
+          <Link to="/" className='home'><FaHome/></Link>
+          
+        </div>
         <div className='logo-text'>
-        Mö·bel
+        
+        <Link to="/" className='logo-text-link'>MOBEL</Link>
 
         </div>
         <div className='nav-links'>
-            <div>Link </div>
-            <div>Link </div>
+            
+            <Link className='nav-links-link' to="/wishlist"><FaHeart color='black'/></Link>
+              <Link className='nav-links-link'to="/cart"><FaShoppingCart color='black'/>
+              </Link>
+              {loggedInUser===null?
+              <Link className='nav-links-link' to="/login">
+              <FaUser color='black'/>
+              </Link>: <Link className='nav-links-link' to='/profile'>
+              <FaUser color="black"/>
+              </Link>
+            }
+
 
         </div>
     </div>
